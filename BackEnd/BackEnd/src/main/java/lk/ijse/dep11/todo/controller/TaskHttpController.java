94925c6358a57f507596023872ec5295a77289fe
udana <udanaoneplus@gmail.com>
2024-01-19 18:10:09 +0530

    Updated Handler Methods parameters/Annotations

--- BackEnd/BackEnd/src/main/java/lk/ijse/dep11/todo/controller/TaskHttpController.java
+++ BackEnd/BackEnd/src/main/java/lk/ijse/dep11/todo/controller/TaskHttpController.java
@@ -2,6 +2,7 @@ package lk.ijse.dep11.todo.controller;
 
 import lk.ijse.dep11.todo.to.TaskTO;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,28 +12,28 @@ import java.util.List;
 @RequestMapping("/api/v1/tasks")
 public class TaskHttpController {
     @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping
-    public TaskTO createTask(){
+    @PostMapping(produces = "application/json",consumes = "application/json")
+    public TaskTO createTask(@RequestBody @Validated TaskTO task){
         System.out.println("This is createTask");
         return null;
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    @PatchMapping
+    @PatchMapping(value = "/{id}", consumes = "application/json")
     public void updateTask(){
         System.out.println("This is updateTask");
 
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    @DeleteMapping
-    public void deleteTask(){
+    @DeleteMapping("/{id}")
+    public void deleteTask(@PathVariable("id") int taskId){
         System.out.println("This is deleteTask");
 
     }
 
-    @GetMapping
-    public List<TaskTO> getAllTasks(){
+    @GetMapping(produces = "application/json", params = {"email"})
+    public List<TaskTO> getAllTasks(String email){
         System.out.println("This is getAllTasks");
         return null;
     }
