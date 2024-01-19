eab156187c28e49bb0f035d48075f7008fa2ab8a
udana <udanaoneplus@gmail.com>
2024-01-19 18:01:34 +0530

    Handler Methods Basic Structure Added

--- BackEnd/BackEnd/src/main/java/lk/ijse/dep11/todo/controller/TaskHttpController.java
+++ BackEnd/BackEnd/src/main/java/lk/ijse/dep11/todo/controller/TaskHttpController.java
@@ -1,4 +1,42 @@
 package lk.ijse.dep11.todo.controller;
 
+import lk.ijse.dep11.todo.to.TaskTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/v1/tasks")
 public class TaskHttpController {
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public TaskTO createTask(){
+        System.out.println("This is createTask");
+        return null;
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PatchMapping
+    public void updateTask(){
+        System.out.println("This is updateTask");
+
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping
+    public void deleteTask(){
+        System.out.println("This is deleteTask");
+
+    }
+
+    @GetMapping
+    public List<TaskTO> getAllTasks(){
+        System.out.println("This is getAllTasks");
+        return null;
+    }
+
+
+
 }
